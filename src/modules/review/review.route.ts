3145e999235e1745b774f14d6734a1b76ac5b31f
Rupok Hossain Siam <rh.siam999@gmail.com>
2026-07-09 01:57:06 +0600

    implement review and rating system with completion check

---
+++ src/modules/review/review.route.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { reviewController } from "./review.controller";
+import { auth } from "../../middleware/auth";
+import { Role } from "../../../generated/prisma";
+
+const router = Router();
+
+
+router.post(
+  "/", 
+  auth(Role.CUSTOMER), 
+  reviewController.createReview
+);
+
+
+router.get("/", reviewController.getAllReviews);
+
+export const reviewRoutes = router;
